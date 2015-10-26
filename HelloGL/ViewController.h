f05f0d4014f167352992cf58376b77d090b736d9
qiang512833564 <512833564@qq.com>
2015-10-26 10:57:19 +0800

    first commit

---
+++ HelloGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGL
+//
+//  Created by lizhongqiang on 15/10/26.
+//  Copyright (c) 2015年 lizhongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
